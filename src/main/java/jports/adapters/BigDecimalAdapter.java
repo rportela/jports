dd38c5fcf9475b720324f08fe52e6f514c3b7434
rportela <rodrigo.portela@hotmail.com>
2019-01-01 22:03:11 -0200

    Big Decimal Adapter

---
+++ src/main/java/jports/adapters/BigDecimalAdapter.java
@@ -0,0 +1,40 @@
+package jports.adapters;
+
+import java.math.BigDecimal;
+
+public class BigDecimalAdapter implements Adapter<BigDecimal> {
+
+	@Override
+	public BigDecimal parse(String source) {
+		return source == null || source.isBlank()
+				? null
+				: new BigDecimal(source);
+	}
+
+	@Override
+	public String format(BigDecimal source) {
+		return source == null
+				? null
+				: source.toString();
+	}
+
+	@Override
+	public BigDecimal convert(Object source) {
+		if (source == null)
+			return null;
+		else if (source instanceof BigDecimal)
+			return ((BigDecimal) source);
+		else if (source instanceof Number)
+			return BigDecimal.valueOf(((Number) source).doubleValue());
+		else if (source instanceof String)
+			return parse((String) source);
+		else
+			throw new RuntimeException("Can't convert to BigDecimal: " + source);
+	}
+
+	@Override
+	public Class<BigDecimal> getDataType() {
+		return BigDecimal.class;
+	}
+
+}
